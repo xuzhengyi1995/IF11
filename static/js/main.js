6981005061ca170417a8ee958c2a699ca7abca51
chriszyyy <zy775355850@vip.qq.com>
2018-06-20 17:41:39 +0200

    Add music detail page, integrate support modal

--- static/js/main.js
+++ static/js/main.js
@@ -1,7 +1,7 @@
 function supportMusician() {
 	var amount = Number($('input[name=amount]:checked').val());
 	if(amount === 1 || amount === 2) {
-		$('#support_confirmation_msg').html('Are you sure to support Yi ZHANG with ' + amount + '€?');
+		$('#support_confirmation_msg').html('Are you sure to support Yifan WU with ' + amount + '€?');
 		$('#support_confirmation_modal').modal('show');
 	} else {
 		$('#support_nomoney_modal').modal('show');
